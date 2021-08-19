e4a87389c85ff0c3c21e1b2d5b468e5096e9e4b6
Sarah Panaligan <sarah-panaligan@unsupervised.com>
2021-08-19 10:39:50 -0600

    testing small changes to message code to see result in slack

--- bot-response/blocks-intro.js
+++ bot-response/blocks-intro.js
@@ -6,14 +6,14 @@ const introBlocks = [
     "type": "section",
     "text": {
       "type": "mrkdwn",
-      "text": ":wave: Hi there! I'm your friendly *Rotation Bot* :arrows_counterclockwise::robot_face: My role is to help track many different rotations across teams."
+      "text": ":wave: Hi there! I'm your friendly *Unsupervised Rotation Bot* :arrows_counterclockwise::robot_face: My role is to help track the On-Point rotations across the Engineering teams."
     }
   },
   {
     "type": "section",
     "text": {
       "type": "mrkdwn",
-      "text": ':speech_balloon: *If you need help with a rotation, post a message to any channel I\'ve been added to and mention `@rota "[rotation-name]" [your message]`*. The person on duty for the rotation will be notified!'
+      "text": ':speech_balloon: *If you need help with an On-Point rotation, post a message to `#general` and mention `@rota "[rotation-name]" [your message]`*. The person on duty for the rotation will be notified!'
     }
   },
   {
