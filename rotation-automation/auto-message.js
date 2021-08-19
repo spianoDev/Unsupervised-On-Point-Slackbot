62fd45b00cfaac00ff8e7d0804e6b8f4468cbe2e
Sarah Panaligan <sarah-panaligan@unsupervised.com>
2021-08-19 16:57:16 -0600

    removed hard coded secret

--- rotation-automation/auto-message.js
+++ rotation-automation/auto-message.js
@@ -1,17 +1,21 @@
 // Require the Node Slack SDK package (github.com/slackapi/node-slack-sdk)
+require('dotenv').config();
+
 const { WebClient, LogLevel } = require("@slack/web-api");
 
 // WebClient instantiates a client that can call API methods
 // When using Bolt, you can use either `app.client` or the `client` passed to listeners.
-const client = new WebClient("xoxb-2396045298034-2419755608016-tdw7s2xplwartA1A1MkSohuQ", {
+const client = new WebClient(process.env.SLACK_BOT_TOKEN, {
     // LogLevel can be imported and used to make debugging simpler
     logLevel: LogLevel.DEBUG
 });
 // list of messages that automatically get sent to slack
 let generalId = "C02BN7337RR"
 let nextAIOnPoint = "<@U02CBN7HW0G> \"ai\" assign next"
+let nextPlatformOnPoint ="<@U02CBN7HW0G> \"platform\" assign next"
 let listOnPoint = "<@U02CBN7HW0G> list"
 let runHelp = "<@U02CBN7HW0G> help"
+let runs = 0;
 //let nextPlatformOnPoint = `<@U02CBN7HW0G> "platform" assign next`
 
 // Post a message to a channel your app is in using ID and message text
@@ -20,7 +24,7 @@ async function publishMessage(id, text) {
         // Call the chat.postMessage method using the built-in WebClient
         const result = await client.chat.postMessage({
             // The token you used to initialize your app
-            token: "xoxb-2396045298034-2419755608016-tdw7s2xplwartA1A1MkSohuQ",
+            token: process.env.SLACK_BOT_TOKEN,
             channel: id,
             text: text
             // You could also use a blocks[] array to send richer content
@@ -34,11 +38,24 @@ async function publishMessage(id, text) {
     }
 }
 
-publishMessage(generalId, runHelp);
-//publishMessage(generalId, nextPlatformOnPoint);
+// publishMessage(generalId, runHelp);
+// publishMessage(generalId, nextAIOnPoint);
+// publishMessage(generalId,listOnPoint);
 
 // Schedule for automatically assigning the next person in the rotation
 
 function scheduleChange(assignmentGroup){
-
+    console.log("Rotation updated for " + assignmentGroup);
+    return publishMessage(generalId, assignmentGroup);
 }
+
+let swaps = setInterval(function(){
+    runs += 1;
+    console.log(runs);
+    if (runs > 2) {
+        clearInterval(swaps);
+    }
+    return scheduleChange(nextAIOnPoint);
+}, 10000);
+
+
