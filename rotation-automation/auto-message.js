81d16865cc21ae0484b91ba1fd5ce0a079ccefe3
Alicia Dale <boricua21dale@gmail.com>
2021-08-19 15:52:34 -0600

    updating bot token

--- rotation-automation/auto-message.js
+++ rotation-automation/auto-message.js
@@ -3,7 +3,7 @@ const { WebClient, LogLevel } = require("@slack/web-api");
 
 // WebClient instantiates a client that can call API methods
 // When using Bolt, you can use either `app.client` or the `client` passed to listeners.
-const client = new WebClient("xoxb-2396045298034-2419755608016-nKsBDHbq0iFicMLRyVeeADcl", {
+const client = new WebClient("xoxb-2396045298034-2419755608016-tdw7s2xplwartA1A1MkSohuQ", {
     // LogLevel can be imported and used to make debugging simpler
     logLevel: LogLevel.DEBUG
 });
@@ -20,7 +20,7 @@ async function publishMessage(id, text) {
         // Call the chat.postMessage method using the built-in WebClient
         const result = await client.chat.postMessage({
             // The token you used to initialize your app
-            token: "xoxb-2396045298034-2419755608016-nKsBDHbq0iFicMLRyVeeADcl",
+            token: "xoxb-2396045298034-2419755608016-tdw7s2xplwartA1A1MkSohuQ",
             channel: id,
             text: text
             // You could also use a blocks[] array to send richer content
